27d44b7de00a54d63158f38a272f70be2387c25c
Roland <roland.onofrej@akademiasovy.sk>
2017-10-19 09:38:57 +0200

    Added new functionalities - arrays

--- random30numbers.cpp
+++ random30numbers.cpp
@@ -56,4 +56,66 @@ srand(time(NULL)); // premiesanie cisel
         min=arr[i];
  }
  printf("\nMin number: %d",min);
+ 
+ printf("\nNumbers that end 0: ");
+ for(i=0;i<N;i++){
+     if(arr[i]%10==0)
+      printf("%d ",arr[i]);
  }
+
+ printf("\nNumbers where last digit is bigger than previous digit: ");
+ for(i=0;i<N;i++){
+     if(arr[i]>=10 && arr[i]%10>arr[i]/10)
+      printf("%d ",arr[i]);
+ }
+ 
+  printf("\nArray from the end: ");
+ for(i=N-1;i>=0;i--){
+      printf("%d ",arr[i]);
+ }
+ 
+  printf("\nEvery second item of the array: ");
+ for(i=0;i<N;i=i+2){
+       printf("%d ",arr[i]);
+ }
+ 
+ printf("\nChanged every 5-th item to the oposit value: ");
+ for(i=0;i<N;i++){              //for(i=4;i<N;i=i+5)
+     if(i%5==4){
+	   arr[i]=-1*arr[i];
+       printf("%d ",arr[i]);
+       }
+ }
+ 
+ int brr[N];   // clone
+ for(i=0;i<N;i++){
+     brr[i]=arr[i];
+ }
+ 
+ int crr[N];  // the same as arr[] from the end
+ for(i=0;i<N;i++){
+     crr[(N-1)-i]=arr[i];
+ }
+ 
+ int drr[N];  // new array
+ for(i=0;i<N;i++){
+     drr[i]=rand()%100;
+ }
+ 
+ int err[N];   // new array -> arr+drr
+ for(i=0;i<N;i++){
+     err[i]=arr[i]+drr[i];
+ }
+ 
+ //print err array
+ printf("\nErr array: ");
+ for(i=0;i<N;i++){
+    printf("%d ",err[i]);
+    }
+    
+ int frr[N];   // new array containd only 0 a 1 - binary array
+ for(i=0;i<N;i++){
+      frr[i]=rand()%2;
+ }
+    
+}
